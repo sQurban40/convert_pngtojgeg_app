4a9c66b91453cc125fe4c0425de9309c2768d000
sQurban <45127669+sQurban40@users.noreply.github.com>
2024-07-08 17:25:28 +0500

    Update convert_pngjpeg_app.py

--- convert_pngjpeg_app.py
+++ convert_pngjpeg_app.py
@@ -1,19 +1,19 @@
 #importing required packages
-import streamlit as st
+import streamlit
 from PIL import Image
 import io
 
 #adding title of the streamlit app
-st.title('PNG to JPEG Converter')
+streamlit.title('PNG to JPEG Converter')
 
 # let user to upload multiple PNG files using File uploader
-uploaded_files = st.file_uploader("Choose PNG files", type="png", accept_multiple_files=True)
+uploaded_files = streamlit.file_uploader("Choose PNG files", type="png", accept_multiple_files=True)
 
 # initializing a dictionary to store converted images
 converted_images = {}
 
 # adding a "Convert" button
-if st.button('Convert'):
+if streamlit.button('Convert'):
     if uploaded_files:
         for uploaded_file in uploaded_files:
             # open the image file usnig Image object of PIL package
@@ -28,13 +28,13 @@ if st.button('Convert'):
 
         # Displaying the converted images and download buttons to allow user to download converted images
         for file_name, converted_image in converted_images.items():
-            st.write(f"Original file: {file_name}")
-            st.image(converted_image, caption='Converted JPEG image', use_column_width=True, output_format="JPEG")
-            st.download_button(
+            streamlit.write(f"Original file: {file_name}")
+            streamlit.image(converted_image, caption='Converted JPEG image', use_column_width=True, output_format="JPEG")
+            streamlit.download_button(
                 label=f"Download {file_name.rsplit('.', 1)[0]}.jpeg",
                 data=converted_image,
                 file_name=f"{file_name.rsplit('.', 1)[0]}.jpeg", on_click=None,
                 mime="image/jpeg"
             )
     else:
-        st.warning("Please upload a PNG file.")
+        streamlit.warning("Please upload a PNG file.")
